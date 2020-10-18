87e54b9293f38a91067a29bfde6c830477ddb8d6
Ronali <ronalipanigrahy88@gmail.com>
2020-10-18 20:53:18 +0530

    CODE FOR RANDOM GENERATION OF HEADS OR TAILS

--- Flipcombination.sh
+++ Flipcombination.sh
@@ -1,3 +1,11 @@
 
-echo "WELCOME MESSAGE ADDED FOR FLIP COMBINATION SIMULATION"
+echo "WELCOME TO FLIP COMBINATION SIMULATION"
 
+flip=$((RANDOM%2))
+if [ $flip -eq 1 ]
+then
+	echo "HEADS"
+elif [ $flip -eq 0 ]
+then
+	echo "TAILS"
+fi
