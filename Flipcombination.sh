4b1ae6e3a8046b01c0145201c7ae85fbc1a7c285
Ronali <ronalipanigrahy88@gmail.com>
2020-10-18 21:39:55 +0530

    STORED THE SINGLETS IN DICTIONARY AND DETERMINED THE SINGLET PERCENTAGES

--- Flipcombination.sh
+++ Flipcombination.sh
@@ -1,11 +1,34 @@
 
 echo "WELCOME TO FLIP COMBINATION SIMULATION"
 
-flip=$((RANDOM%2))
-if [ $flip -eq 1 ]
-then
-	echo "HEADS"
-elif [ $flip -eq 0 ]
-then
-	echo "TAILS"
-fi
+declare -A singlet
+
+heads=0
+tails=0
+read -p "HOW MANY TIMES YOU WANT TO FLIP THE COIN" n
+
+for((i=1;i<=$n;i++))
+do
+     flip=$((RANDOM%2))
+     singlet[$i]=$flip
+     if [ $flip -eq 1 ]
+     then
+	#echo "HEADS"
+	heads=$(($heads+1))
+     elif [ $flip -eq 0 ]
+     then
+	#echo "TAILS"
+	tails=$(($tails+1))
+
+    fi
+done
+
+echo "STORED IN singlet DICTIONARY"
+
+echo ${singlet[@]}
+echo "HEADS="$heads
+echo "TAILS="$tails
+hp=$(((heads*100)/$n))
+tp=$(((tails*100)/$n))
+echo "HEAD PERCENTAGE IS $hp% "
+echo "TAIL PERCENTAGE IS $tp%"
